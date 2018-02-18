fcdb6b16e869a0adf0f6d737f27fac647d1c6380
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-02-18 18:02:42 +0100

    Adding def for other boards
    - OPIZ (Orange Pi Zero)
    - UNIPI (Neuron S103-G, LED and BUTTON are not used)
    - adding macros for TR IO1 and IO2 pins

--- include/machines_def.h
+++ include/machines_def.h
@@ -12,7 +12,40 @@
 #define RESET_GPIO (23)
 /** SPI CE GPIO. */
 #define RPIIO_PIN_CE0 (8)
-
+/** TR IO1 GPIO. */
+#define IO1_GPIO (24)
+/** TR IO2 GPIO. */
+#define IO2_GPIO (25)
 #endif /* RPI */
+
+#ifdef OPIZ
+/** LED GPIO. */
+#define LED_GPIO (3)
+/** Button GPIO. */
+#define BUTTON_GPIO (10)
+/** Reset GPIO. */
+#define RESET_GPIO (19)
+/** SPI CE GPIO. */
+#define RPIIO_PIN_CE0 (13)
+/** TR IO1 GPIO. */
+#define IO1_GPIO (18)
+/** TR IO2 GPIO. */
+#define IO2_GPIO (2)
+#endif /* OPI */
+
+#ifdef UNIPI
+/** LED GPIO - NOT USED. */
+#define LED_GPIO (0)
+/** Button GPIO - NOT USED. */
+#define BUTTON_GPIO (0)
+/** Reset GPIO. */
+#define RESET_GPIO (18)
+/** SPI CE GPIO. */
+#define RPIIO_PIN_CE0 (8)
+/** TR IO1 GPIO. */
+#define IO1_GPIO (17)
+/** TR IO2 GPIO. */
+#define IO2_GPIO (4)
+#endif /* UNIPI */
 
 #endif /* __MACHINES_DEF_H */ 
