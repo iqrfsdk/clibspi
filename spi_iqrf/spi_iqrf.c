eca936b9645659ea99f7b84ea7579da7371f726c
MacHut-coder <dusan.machut@iqrf.com>
2018-06-05 16:33:43 +0000

    PGM mode entry function modified

--- spi_iqrf/spi_iqrf.c
+++ spi_iqrf/spi_iqrf.c
@@ -1507,37 +1507,55 @@ int spi_iqrf_pe(void)
       return BASE_TYPES_OPER_ERROR;
     }
 
-    strcpy(sysCommand, "modprobe -r ");
-    strcat(sysCommand, spiIqrfConfig->spiKernelModule);
-    system(sysCommand);
+    // if SPI peripheral kernel module exist
+    if (strlen(spiIqrfConfig->spiKernelModule) != 0){
+      strcpy(sysCommand, "modprobe -r ");
+      strcat(sysCommand, spiIqrfConfig->spiKernelModule);
+      system(sysCommand);
 
-//    gpio_setup(spiIqrfConfig->spiMisoGpioPin, GPIO_DIRECTION_IN, 0);
-//    gpio_setup(spiIqrfConfig->spiMosiGpioPin, GPIO_DIRECTION_IN, 0);
-    gpio_setup(spiIqrfConfig->spiPgmSwGpioPin, GPIO_DIRECTION_OUT, 1);
+      gpio_setup(spiIqrfConfig->spiMisoGpioPin, GPIO_DIRECTION_IN, 0);
+      gpio_setup(spiIqrfConfig->spiMosiGpioPin, GPIO_DIRECTION_IN, 0);
+      gpio_setup(spiIqrfConfig->spiPgmSwGpioPin, GPIO_DIRECTION_OUT, 1);
 
-    if (spi_reset_tr() != BASE_TYPES_OPER_OK) {
-        return BASE_TYPES_OPER_ERROR;
+      if (spi_reset_tr() != BASE_TYPES_OPER_OK) {
+          return BASE_TYPES_OPER_ERROR;
+      }
+
+      gpio_setup(spiIqrfConfig->spiCe0GpioPin, GPIO_DIRECTION_OUT, 0);
+
+      // Sleep for 500ms
+      SLEEP(500);
+
+      gpio_setDirection(spiIqrfConfig->spiPgmSwGpioPin, GPIO_DIRECTION_IN);
+      gpio_setValue(spiIqrfConfig->spiCe0GpioPin, 1);
+
+      // Sleep for 100ms
+      SLEEP(100);
+
+      gpio_cleanup(spiIqrfConfig->spiCe0GpioPin);
+      gpio_cleanup(spiIqrfConfig->spiMisoGpioPin);
+      gpio_cleanup(spiIqrfConfig->spiMosiGpioPin);
+
+      // Init SPI
+      strcpy(sysCommand, "modprobe ");
+      strcat(sysCommand, spiIqrfConfig->spiKernelModule);
+      system(sysCommand);
     }
+    else {
+      gpio_setup(spiIqrfConfig->spiPgmSwGpioPin, GPIO_DIRECTION_OUT, 1);
 
-//    gpio_setup(spiIqrfConfig->spiCe0GpioPin, GPIO_DIRECTION_OUT, 0);
+      if (spi_reset_tr() != BASE_TYPES_OPER_OK) {
+          return BASE_TYPES_OPER_ERROR;
+      }
 
-    // Sleep for 500ms
-    SLEEP(500);
+      // Sleep for 500ms
+      SLEEP(500);
 
-    gpio_setDirection(spiIqrfConfig->spiPgmSwGpioPin, GPIO_DIRECTION_IN);
-//    gpio_setValue(spiIqrfConfig->spiCe0GpioPin, 1);
+      gpio_setDirection(spiIqrfConfig->spiPgmSwGpioPin, GPIO_DIRECTION_IN);
 
-    // Sleep for 100ms
-    SLEEP(100);
-
-//    gpio_cleanup(spiIqrfConfig->spiCe0GpioPin);
-//    gpio_cleanup(spiIqrfConfig->spiMisoGpioPin);
-//    gpio_cleanup(spiIqrfConfig->spiMosiGpioPin);
-
-    // Init SPI
-    strcpy(sysCommand, "modprobe ");
-    strcat(sysCommand, spiIqrfConfig->spiKernelModule);
-    system(sysCommand);
+      // Sleep for 100ms
+      SLEEP(100);
+    }
 
     if (spi_iqrf_open() != BASE_TYPES_OPER_OK) {
       return BASE_TYPES_OPER_ERROR;
